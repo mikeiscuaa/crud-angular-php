3595b42973b0164cbbbfe9982e039f727e40a0b1
MikeUaaIsc <ing.miguel.o.e@gmail.com>
2024-04-23 14:08:20 -0600

    Inserta los datos en la base de datos de MySQL

--- src/app/components/agregar-libro/agregar-libro.component.ts
+++ src/app/components/agregar-libro/agregar-libro.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
-import {Route, Router } from '@angular/router';
-// importar el servicio de crud
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { CrudService } from '../../service/crud.service';
 
 @Component({
   selector: 'app-agregar-libro',
@@ -12,18 +12,23 @@ export class AgregarLibroComponent {
   // Recolectamos los datos desde el 
   formularioDeLibros: FormGroup;
 
-  constructor(public formulario:FormBuilder){
-    this.formularioDeLibros=this.formulario.group({
-      isbn:[''],
-      titulo:[''],
-      autor:[''],
-      anio:[''],
-      editorial:['']
+  constructor(public formulario: FormBuilder, private crudService: CrudService,
+    private ruteador: Router) {
+
+    this.formularioDeLibros = this.formulario.group({
+      isbn: [''],
+      titulo: [''],
+      autor: [''],
+      anio: [''],
+      editorial: ['']
     });
   }
-  enviarDatos(){
-    console.log("Presionaste el boton enviar datos, quieres enviar datos?");
-    console.log(this.formularioDeLibros.value);
+  enviarDatos() {
+    // console.log("Presionaste el boton enviar datos, quieres enviar datos?");
+    // console.log(this.formularioDeLibros.value);
+    this.crudService.AgregarLibro(this.formularioDeLibros.value).subscribe(respuesta => {
+      this.ruteador.navigateByUrl('/listar-libro');
+    })
 
   }
 }
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Libro } from './Libros';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
+  API: string = "http://localhost/libros/";
 
-  constructor() { }
+  constructor(private clienteHttp: HttpClient) {}
+    
+  AgregarLibro(datoslibro: Libro){
+    return this.clienteHttp.post(this.API + "?insertar=1", datoslibro);
+    }
+   
 }
