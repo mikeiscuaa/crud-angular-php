27d8a44926adcd275a537e56ea94e1e6e9b431b9
MikeUaaIsc <ing.miguel.o.e@gmail.com>
2024-04-30 14:34:51 -0600

    CRUD terminado

--- src/app/components/listar-libro/listar-libro.component.ts
+++ src/app/components/listar-libro/listar-libro.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../../service/crud.service';
-import { NgFor, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-listar-libro',
@@ -14,8 +13,22 @@ export class ListarLibroComponent implements OnInit {
   ngOnInit(): void {
     this.crudService.ObtenerLibros().subscribe(respuesta => {
       console.log(respuesta);
-      this.Libros=respuesta;
+      this.Libros = respuesta;
     })
 
   }
+
+  borrarRegistro(iControl: any, isbn: any, titulo: any)
+  {
+    // console.log(iControl);
+    // console.log(isbn)
+    // console.log(titulo)
+    if(window.confirm("Realmente deseas eliminar el libro con titulo "+titulo)){
+      this.crudService.BorrarLibro(isbn).subscribe(respuesta=>{
+        // Esto es para actualizar la lista de libros en el template
+        this.Libros.splice(iControl,1);
+      });
+    }
+  }
+
 }
